02ed82480756c2bb3c7effec9dce2b8a7d4952cc
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2024-05-13 03:44:36 +0300

    DB added

---
+++ Rent-Premises/RentPremises.DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Rent_Premises.DAL.Entities;
+using Rent_Premises.DAL.Entities.Base;
+
+namespace Rent_Premises.DAL.Context;
+
+public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
+    public virtual DbSet<Lessor> Lessors { get; set; }
+    public virtual DbSet<Renter> Renters { get; set; }
+    public virtual DbSet<Premises> Premises { get; set; }
+    public virtual DbSet<TypeOfPremises> TypeOfPremises { get; set; }
+    
+}
---
+++ Rent-Premises/RentPremises.DAL/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Rent_Premises.DAL.Context;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(
+            "Server=localhost;Database=RentPremises;Trusted_Connection=True;TrustServerCertificate=True");
+        
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+    
+    
+}
