4d1d5201cd22d621b01b4d15defe891540553305
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2024-05-13 03:43:49 +0300

    Create Renter.cs

---
+++ Rent-Premises/RentPremises.DAL/Entities/Renter.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Rent_Premises.DAL.Entities.Base;
+
+namespace Rent_Premises.DAL.Entities;
+
+public class Renter : BaseEntity<Guid>
+{
+    public Guid UserId { get; set; }
+    
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+    public List<Premises> RentedPremises { get; set; } = new List<Premises>();
+}
